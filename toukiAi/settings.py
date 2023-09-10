70a8dff0c39b88aa5c36d3b4f7ede1e4189e92d8
SagaTatsuya <t.saga@yahoo.ne.jp>
2023-09-11 08:56:36 +0900

    for_deploy

--- toukiAi/settings.py
+++ toukiAi/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = "django-insecure-mb&-%9clmw_gpl1e6=0h3tlg3r=@o08ja75+21b880n&l1xq7t"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
@@ -104,11 +105,10 @@ WSGI_APPLICATION = 'toukiAi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+default_dburl = "sqlite:///" + str(BASE_DIR / "db.sqlite3")
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": config("DATABASE_URL", default=default_dburl, cast=dburl),
 }
 
 

d6798a096fc85e3b2b95f173582a22dcb2ab120d
SagaTatsuya <t.saga@yahoo.ne.jp>
2023-10-10 02:42:39 +0900

    update_header_responsive

--- toukiAi/static/header.js
+++ toukiAi/static/header.js
@@ -19,6 +19,8 @@ const toInquiryBtn = document.getElementById("toInquiryBtn");
 const logoutBtn = document.getElementById("logoutBtn");
 const headerBtns = [toIntroBtn, toSupportBtn, toChargeBtn, toCompareBtn, toQABtn, toInquiryBtn, logoutBtn];
 
+const toggler = document.getElementById("toggler");
+
 /*
     index以外のページにいるときにヘッダーにあるボタンが押されたとき、
     どのボタンが押されたかを取得してindexに移動する
@@ -50,15 +52,25 @@ window.addEventListener("load",()=>{
         if(i < headerBtns.length - 1){
             headerBtns[i].addEventListener("click",(e)=>{
                 moveToIndex(location.pathname, e);
+
+                const closeBtn = header.getElementsByClassName("btn-close")[0];
+                const event = new Event("click");
+                closeBtn.dispatchEvent(event);
             })
         }else{
-            headerBtns[i].addEventListener("click",(e)=>{
-                const date1 = new Date();
-                const date2 = date1.getHours() + "時" + 
-                            date1.getMinutes() + "分" + 
-                            date1.getSeconds() + "秒"
-                sessionStorage.setItem("lastUpdateDate", date2);
-            })
+            if(headerBtns[i] !== null){
+                headerBtns[i].addEventListener("click",(e)=>{
+                    const date1 = new Date();
+                    const date2 = date1.getHours() + "時" + 
+                                date1.getMinutes() + "分" + 
+                                date1.getSeconds() + "秒"
+                    sessionStorage.setItem("lastUpdateDate", date2);
+                })
+            }
         }
     }
+
+    toggler.addEventListener("click",(e)=>{
+        //
+    })
 })

a9def2a439f9b99231218bb94c7f786dc17cef36
SagaTatsuya <t.saga@yahoo.ne.jp>
2024-02-24 09:20:17 +0900

    master_toukikun.yml_fix_node.js_version

--- toukiAi/settings/local.py
+++ toukiAi/settings/local.py
@@ -34,24 +34,13 @@ EMAIL_HOST_USER = 'toukiaidev@gmail.com'
 EMAIL_HOST_PASSWORD = 'dmpwozrseacxyagh'
 EMAIL_USE_TLS = True 
 
-# DATABASES = {
-#     'default':{
-#         'ENGINE':'django.db.backends.postgresql',
-#         'NAME':'toukiai',
-#         'USER':'tatsuyasaga',
-#         'PASSWORD':'saga2497',
-#         'HOST':'localhost',
-#         'PORT':'5432',
-#     }
-# }
-
 DATABASES = {
     'default':{
         'ENGINE':'django.db.backends.postgresql',
-        'NAME':'postgres',
-        'USER':'souzokutoukikun',
-        'PASSWORD':'111111a;',
-        'HOST':'souzokutoukikun.postgres.database.azure.com',
+        'NAME':'toukiai',
+        'USER':'tatsuyasaga',
+        'PASSWORD':'saga2497',
+        'HOST':'localhost',
         'PORT':'5432',
     }
 }

1f40a4c7d58527fce03db00e455b6df171ed873f
SagaTatsuya <t.saga@yahoo.ne.jp>
2024-03-25 04:02:06 +0900

    fix

--- build.sh
+++ build.sh
@@ -2,6 +2,15 @@
 # exit on error
 set -o errexit
 
+# Rustのインストール
+# Rustupスクリプトをダウンロードして実行
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+# 環境変数PATHにRustのパスを追加
+# このスクリプト内でのみ有効
+export PATH="$HOME/.cargo/bin:$PATH"
+
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input

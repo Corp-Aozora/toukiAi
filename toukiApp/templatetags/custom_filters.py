2f76aeef17f117648abad763fdc0db2f15636989
SagaTatsuya <t.saga@yahoo.ne.jp>
2024-04-14 05:30:17 +0900

    update_index

--- toukiApp/templatetags/custom_filters.py
+++ toukiApp/templatetags/custom_filters.py
@@ -1,4 +1,5 @@
 from django import template
+import unicodedata
 
 register = template.Library()
 
@@ -22,17 +23,27 @@ def after(value, arg):
 
 @register.filter
 def addstr(arg1, arg2):
-    """concatenate arg1 & arg2"""
+    """文字連結"""
     return str(arg1) + str(arg2)
 
 @register.filter
 def to_full_width(value):
+    """数字を全角にする"""
     return str(value).translate(str.maketrans('0123456789', '０１２３４５６７８９'))
 
+@register.filter(name='to_half_width_digits')
+def to_half_width_digits(value):
+    """文字列中の全角数字を半角数字に変換"""
+    normalized = unicodedata.normalize('NFKC', value)
+    # 特別な文字の置換（全角カンマを半角カンマに）
+    return normalized.replace('，', ',')
+    
 @register.filter
 def get_item(list, index):
+    """リストから対象の要素を取得する"""
     return list[index]
 
 @register.filter(name='classname')
 def classname(obj):
+    """クラス名を取得する"""
     return obj.__class__.__name__

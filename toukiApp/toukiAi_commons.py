5a18b68f80815d1d3987dd200aa26a32077703f9
SagaTatsuya <t.saga@yahoo.ne.jp>
2024-09-05 10:35:50 +0900

    suspend_auto_email

--- toukiApp/toukiAi_commons.py
+++ toukiApp/toukiAi_commons.py
@@ -478,44 +478,44 @@ ANSWER_EMAIL_TEMPLATE = textwrap.dedent('''
     ----------お問い合わせ内容----------
 ''') + ANSWER_TO_INQUIRY_EMAIL_TEMPLATE
 
-from email.mime.text import MIMEText
-import smtplib
-import base64
+# from email.mime.text import MIMEText
+# import smtplib
+# import base64
 
-def send_auto_email_to_inquiry(cleaned_data, to_email, is_user=True):
-    mail_subject = f"{CompanyData.APP_NAME}からの自動返信です"
-    category = cleaned_data.get("category", "")
-    content = AUTO_REPLY_EMAIL_TEMPLATE.format(
-        inquiry_category = Sections.get_category(category) + "の" if category else "",
-        inquiry_subject = Sections.get_subject(cleaned_data["subject"]) if is_user else cleaned_data["subject"],
-        content = cleaned_data["content"], 
-        company_name = CompanyData.NAME,
-        company_post_number = CompanyData.POST_NUMBER,
-        company_address = CompanyData.ADDRESS,
-        company_bldg = CompanyData.BLDG,
-        company_receiving_phone_number = CompanyData.RECEIVING_PHONE_NUMBER,
-        company_calling_phone_number = CompanyData.CALLING_PHONE_NUMBER,
-        company_opening_hours = CompanyData.OPENING_HOURS,
-        company_url = CompanyData.URL,
-    )
+# def send_auto_email_to_inquiry(cleaned_data, to_email, is_user=True):
+#     mail_subject = f"{CompanyData.APP_NAME}からの自動返信です"
+#     category = cleaned_data.get("category", "")
+#     content = AUTO_REPLY_EMAIL_TEMPLATE.format(
+#         inquiry_category = Sections.get_category(category) + "の" if category else "",
+#         inquiry_subject = Sections.get_subject(cleaned_data["subject"]) if is_user else cleaned_data["subject"],
+#         content = cleaned_data["content"], 
+#         company_name = CompanyData.NAME,
+#         company_post_number = CompanyData.POST_NUMBER,
+#         company_address = CompanyData.ADDRESS,
+#         company_bldg = CompanyData.BLDG,
+#         company_receiving_phone_number = CompanyData.RECEIVING_PHONE_NUMBER,
+#         company_calling_phone_number = CompanyData.CALLING_PHONE_NUMBER,
+#         company_opening_hours = CompanyData.OPENING_HOURS,
+#         company_url = CompanyData.URL,
+#     )
 
-    from_email = settings.DEFAULT_FROM_EMAIL
-    message = MIMEText(content)
-    message['Subject'] = mail_subject
-    message['From'] = from_email
-    message['To'] = to_email
-    message['Bcc'] = settings.DEFAULT_FROM_EMAIL
+#     from_email = settings.DEFAULT_FROM_EMAIL
+#     message = MIMEText(content)
+#     message['Subject'] = mail_subject
+#     message['From'] = from_email
+#     message['To'] = to_email
+#     message['Bcc'] = settings.DEFAULT_FROM_EMAIL
 
-    access_token = get_oauth2_access_token()
+#     access_token = get_oauth2_access_token()
 
-    smtp_server = "smtp.gmail.com"
-    smtp_port = 587
+#     smtp_server = "smtp.gmail.com"
+#     smtp_port = 587
 
-    with smtplib.SMTP(smtp_server, smtp_port) as server:
-        server.starttls()
-        auth_string = f"user={from_email}\1auth=Bearer {access_token}\1\1"
-        server.docmd("AUTH", "XOAUTH2 " + base64.b64encode(auth_string.encode()).decode())
-        server.send_message(message)
+#     with smtplib.SMTP(smtp_server, smtp_port) as server:
+#         server.starttls()
+#         auth_string = f"user={from_email}\1auth=Bearer {access_token}\1\1"
+#         server.docmd("AUTH", "XOAUTH2 " + base64.b64encode(auth_string.encode()).decode())
+#         server.send_message(message)
 
 
 # def send_auto_email_to_inquiry(cleaned_data, to_email, is_user=True):
